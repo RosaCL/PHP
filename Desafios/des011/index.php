96ba1340dc3d15d0728238177b1776e05ebb8ff3
Rosa <costurezati@gmail.com>
2025-03-21 07:00:49 -0300

    .

--- Desafios/des011/index.php
+++ Desafios/des011/index.php
@@ -16,10 +16,29 @@
     $desconto=$preco-$formula;
     ?>
     <main>
-        <form action="">
-            <input type="submit" value="Enviar">
+        <h1>Reajustador de preços</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+
+            <label for="preco">Preço</label>
+            <input type="number" name="preco" id="preco" min="0.10" step="0.01" >
+            <label for="porc">Qual será o percentual de reajuste? <strong><span id="p">%</span></strong></label>
+            <input type="range" name="porc" id="porc" min="0" max="100" step="1" oninput="mudaValor()">
+            <input type="submit" value="Reajustar">
         </form>
+       
+       
     </main>
-    
+    <section>
+        <h2>Resultado do reajuste</h2>
+        <p>O produto que custava <strong>R$ <?=$preco?></strong>, com <strong><?=$porc?>% de aumento</strong> vai passar a custar <strong>R$<?=$soma?></strong> a partir de agora.
+        </p>
+
+    </section>
+   <script>
+    mudaValor()
+    function mudaValor(){
+        p.innerText=porc.value;
+    }
+   </script> 
 </body>
 </html>
